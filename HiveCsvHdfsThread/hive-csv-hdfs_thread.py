e16a92eed65d678a01bee55976997cc34cf8bcb7
lingyuguo <30824318+lingyuguo@users.noreply.github.com>
2019-02-28 09:33:45 +0800

    Update hive-csv-hdfs_thread.py

--- HiveCsvHdfsThread/hive-csv-hdfs_thread.py
+++ HiveCsvHdfsThread/hive-csv-hdfs_thread.py
@@ -8,28 +8,31 @@ from PoolTask import Pool,Task   #多线程
 from TCLIService.ttypes import TOperationState
 
 
-table_name = ["battery_pack","test"] #数据表
-HIVE_IP = "192.168.132.3"
-HDFS_IP = "192.168.132.9:50070"
-local_csv_dir = "/home/guolingyu"
+tablename = "test" #数据表
+HIVE_IP = "****"
+HDFS_IP = "***"
+local_csv_dir = "****"
+pool_size = 5
+
 
 #csv格式
-def hive_to_csv(tablename):
-	file = open("%s.csv"%tablename, "w")
-        
-	#headers = ["time","value1","value2","value3"]
+def hive_to_csv(row):
+        start = time.time()
+        file = open("%s%s%s.csv"%(local_csv_dir,tablename,row), "w")  
+        #headers = ["time","value1","value2","value3"]
         #f_csv = csv.DictWriter(file, headers)  #标题行
 	#f_csv.writeheader()
-
-	row = pyhive_query(tablename)
-	file = open("%s.csv"%tablename, "w") 
+	
 	writer = csv.writer(file)
-	writer.writerows(row)
-	file.close()
-	print "%s导出成功!数据%d行"%(tablename,len(row))
+        row_wri = rows[row*100000:(row+1)*100000]
+        writer.writerows(row_wri)
+        file.close()
+        end = time.time()
+        print "%s%s%s.csv write time %0.2f s\n"%(local_csv_dir,tablename,row,end-start)
+
 #hive查询
-def pyhive_query(tablename):
-	sql = 'select * from default.%s limit 4'%tablename
+def pyhive_query():
+	sql = 'select * from default.%s'%tablename
 	cursor = hive.connect(HIVE_IP).cursor()
 	cursor.execute(sql, async=True)
 	#得到语句查询状态
@@ -45,33 +48,34 @@ def pyhive_query(tablename):
     	        rows = ('null')
 		return rows
 	else:
+		print "%s table count %d row"%(tablename,len(rows))
 		return rows
 	# 关闭hive连接
     	cursor.close()
     	#hiveConn.close()
 
 #导入到HDFS
-def csv_to_hdfs(tablename):
-	client = HdfsClient(hosts=HDFS_IP)
-	client.copy_from_local('%s/%s.csv'%(local_csv_dir,tablename), '/hivecsv-%s'%tablename) #本地文件绝对路径,HDFS目录必须不存在
+def csv_to_hdfs(row):
+        client = HdfsClient(hosts=HDFS_IP)
+        client.copy_from_local("%s%s%s.csv"%(local_csv_dir,tablename,row), '/hivecsv-%s%s'%(tablename,row))  #本地文件绝对路径,HDFS目录必须不存在
 
 
 #多线程
-def exe(tablename):
-	rLock = threading.RLock()
-	rLock.acquire()
-	table_name.remove(tablename)
-	rLock.release()
-	hive_to_csv(tablename)
-	#csv_to_hdfs(tablename)
-		
 def main():
-	pool = Pool(size=5)
-	pool.add_tasks([(exe, (tablename,)) for tablename in table_name])
-	pool.run()	
+        csv_start = time.time()
+        pool = Pool(size=pool_size)
+        pool.add_tasks([(hive_to_csv, (row,)) for row in range(len(rows)/100000+1)])
+        pool.run()
+        csv_end = time.time()
+        print "csv write all spend %0.2f s"%(csv_end-csv_start)
 
 if __name__ == "__main__":
-	while len(table_name) != 0:
-		print("查询数据开始")
-		main()
-		print("查询数据结束")
+
+        print("查询数据开始")
+        query_start = time.time()
+        rows = pyhive_query()
+        query_end = time.time()
+        print "hive query time %0.2f s"%(query_end-query_start)
+        main()
+        print("查询数据结束")
+		
