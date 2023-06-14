67dbbf433dcba3bbdb1c87bc53535b484d8534da
lingyuguo <30824318+lingyuguo@users.noreply.github.com>
2023-06-14 16:38:42 +0800

    Update sendmail.py

--- template/sendmail.py
+++ template/sendmail.py
@@ -3,8 +3,8 @@ import smtplib
 import string 
 HOST = "smtp.163.com"
 SUBJECT = "Test emaile from python" 
-TO = "986702758@qq.com"
-FROM = "lingyuguo6240@163.com"
+TO = "**@qq.com"
+FROM = "**@163.com"
 text = "Python rules them all !"
 BODY = string.join ((
 		"FROM : %s"%FROM,
@@ -16,6 +16,6 @@ BODY = string.join ((
 server = smtplib.SMTP()
 server.connect (HOST,"25")
 server.starttls()
-server.login("lingyuguo6240@163.com","******") 
+server.login("***@163.com","******") 
 server.sendmail(FROM,[TO],BODY)
 server.quit() 
